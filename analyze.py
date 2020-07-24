sum_of_squares calculation fix analyze.py update

analyze.py
@@ -1,15 +1,17 @@
 import numpy as np
 import torch
-from tqdm import tqdm
 from scipy.spatial.distance import pdist, squareform
+from tqdm import tqdm
+import os
 from os.path import join
+import pandas as pd
 import json
 
 from IDNN.intrinsic_dimension import estimate, block_analysis
 import sum_of_squares
 import rsa
 
-# to compute ID, SS and RSA on extracted activations
+# compute ID, SS (and RSA) on extracted activations
 
 def computeID(r, number_resampling=50, fraction=0.9, distance_metric='euclidean'):
     ID = []
@@ -26,18 +28,6 @@ def computeID(r, number_resampling=50, fraction=0.9, distance_metric='euclidean'
     return mean, error
 
 
-# sort models by training-logic for plotting
-def model_sort(model_list):
-    model_order = ['0batch1', '0batch3', '0batch10', '0batch30', '0batch100', '0batch300', '1', '3', '10', '30', '100']
-    sorted_models = []
-    for i in range(len(model_order)):
-        for key in model_list:
-            if key.endswith(join(model_order[i] + '.pt')):
-                sorted_models.append(key)
-    return sorted_models
-
-
-
 if __name__ == '__main__':
     # set device
     if torch.cuda.is_available():
@@ -47,27 +37,40 @@ if __name__ == '__main__':
         device = torch.device("cpu")
         print("Devise used = ", device)
 
-    for i in range(1,11): # for 10 seed folders
-        path = join('/mnt/antares_raid/home/bemmerl/thesis/data/mnist2class/models_' + str(i)) #####
-        models = torch.load(join(path, '_extracted.pt')) #map_location='cpu'
+    trained_dataset = 'mnist'  # trained on
+    extract_dataset = 'mnist'  # extracted on
+    root_dir = os.getcwd()
+    models_dir = join(root_dir, 'models', trained_dataset)
+
+    for seed in range(1, 11):  # for 10 seed folders
+        print(f'>>Seed {seed} / pre-trained on {trained_dataset}, extracted on {extract_dataset}<<<')
+        path = join(models_dir, 'models_' + str(seed))
+        extract = torch.load(join(path, extract_dataset + '_extracted.pt'))  # map_location='cpu'
+
         all_ids = []
         all_ss = []
-        for name, model in tqdm(models.items()):
+        for name, model in tqdm(extract.items()):
             print(name)
             layers = model['layers']  # input + 6 model output layers
             labels = model['labels']
+
             ids = []
             ss = []
             for layer in layers:
                 id, error = computeID(layer)
                 ids.append(id)
-                tss, ss_mean, ssw = sum_of_squares.sum_squared(layer, labels)
-                ss.append(ssw / tss if tss != 0 else 0)  ## alternative with ssw
+                tss, ssw = sum_of_squares.sum_squared(layer, labels)
+                ss.append(ssw / tss if tss != 0 else 0)
 
             all_ids.append(ids)
             all_ss.append(ss)
 
-        results = zip(models.keys(), all_ss, all_ids)
+
+        ## df
+
+
+
+        results = zip(extract.keys(), all_ss, all_ids)
         print(list(results))
         order = ['0batch1', '0batch3', '0batch10', '0batch30', '0batch100', '0batch300', '1', '3', '10', '30', '100']
         new_list = list()
@@ -79,7 +82,7 @@ if __name__ == '__main__':
         #                 break
 
         for i in range(len(order)):
-            for line in zip(models.keys(), all_ss, all_ids):
+            for line in zip(extract.keys(), all_ss, all_ids):
                 if line[0].endswith(join('_' + order[i] + '.pt')):
                     new_list.append(line)
                     break
@@ -87,7 +90,7 @@ if __name__ == '__main__':
         print('---- 0batch1 ?: ', new_list[0:11])
         print('---- 0batch3 ?: ', new_list[11:22])
         print('---- 0batch10 ?: ', new_list[22:33])
-
+        break
         analytics = {
             'model_pre_mnist2': new_list,
         }
@@ -105,7 +108,7 @@ if __name__ == '__main__':
         #     'model_ft_mnist2_mnist_30': new_list[99:110],
         #     'model_ft_mnist2_mnist_100': new_list[110:121]
         #     }
-        with open(join(path, 'ss_id2.json'), 'w+') as f:
+        with open(join(path, 'ss_id.json'), 'w+') as f:
             json.dump(analytics, f)
 
 # output dict keys = pretrained model finetuned and value = ('model checkpoint', [ss for 7 layers], [id for 7 layers])

rsa.py
@@ -1,20 +1,19 @@
 import torch
 import numpy as np
-import scipy.stats
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
+import pandas as pd
+import dill
+import time
+import math
 import os
+from os.path import join
+import scipy.stats
 from scipy.spatial import distance
 from scipy.stats import spearmanr
 from sklearn.metrics.pairwise import manhattan_distances
-from os.path import join
 from tqdm import tqdm
 from natsort import natsorted
-import dill
-import pickle
-import time
-import math
-import seaborn as sns
 
 
 def correlationd_matrix(activations):
@@ -84,14 +83,13 @@ def calc_target_compare_rdm(dist_fun, corr_dist_dict_source, corr_dist_dict_targ
     return compare_rdm_diagonal
 
 
-def plot_rdm_compare(compare_rdm_euclid, compare_rdm_spearman, model_names):
+def plot_rdm_compare(compare_rdm_euclid, compare_rdm_spearman, model_names, acc):
     fig, ax1 = plt.subplots(figsize=(8, 8), dpi=150)
-    # ax1.set_xticks(model_names)
-    ax1.xticks(range(0, len(model_names)), model_names, rotation=80)
+    ax1.tick_params(axis='x', labelsize=8, labelrotation=60)
     ax1.set_xlabel('Models')
 
-    ax1.set_ylabel('euclidian distance', color='r')
-    ax1.plot(model_names, compare_rdm_euclid, label='euclidian', color='r')
+    ax1.set_ylabel('euclidean distance', color='r')
+    ax1.plot(model_names, compare_rdm_euclid, label='euclidean', color='r')
     ax1.tick_params(axis='y', labelcolor='r')
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
@@ -101,13 +99,12 @@ def plot_rdm_compare(compare_rdm_euclid, compare_rdm_spearman, model_names):
 
     ax3 = ax1.twinx()
     # Make some space on the right side for the extra y-axis.
-    fig.subplots_adjust(right=0.75)
+    fig.subplots_adjust(right=0.65)
     # Move the last y-axis spine over to the right by 20% of the width of the axes
-    ax3.spines['right'].set_position(('axes', 1.3))
+    ax3.spines['right'].set_position(('axes', 1.2))
     ax3.set_frame_on(True)
     ax3.patch.set_visible(False)
 
-    acc = [0.1, 0.2, 0.3, 0.5, 0.7, 0.8, 0.85, 0.9, 0.94, 0.95, 0.96]
     ax3.set_ylabel('Accuracy', color='g')
     ax3.plot(model_names, acc, marker='o', linestyle='none', label='Acc', color='g')
     ax3.tick_params(axis='y', labelcolor='g')
@@ -118,7 +115,7 @@ def plot_rdm_compare(compare_rdm_euclid, compare_rdm_spearman, model_names):
 
 def plot_rdm(rdm, model_names):
     fig = plt.figure(figsize=(16, 14), dpi=150)
-    ax = seaborn.heatmap(rdm, cmap='rainbow', annot=True, fmt='6.3g', xticklabels=model_names, yticklabels=model_names,
+    ax = sns.heatmap(rdm, cmap='rainbow', annot=True, fmt='6.3g', xticklabels=model_names, yticklabels=model_names,
                          vmin=0, vmax=210)
     fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
 
@@ -129,8 +126,8 @@ def multi_plot_rdm(corr_dict, labels):
     fig.subplots_adjust(hspace=.5, wspace=.001)
 
     for ax, val in tqdm(zip(axs.ravel(), corr_dict.items())):
-        seaborn.set(font_scale=0.8)
-        seaborn.heatmap(val[1], cmap='rainbow', ax=ax, cbar=True, xticklabels=labels, yticklabels=labels)
+        sns.set(font_scale=0.8)
+        sns.heatmap(val[1], cmap='rainbow', ax=ax, cbar=True, xticklabels=labels, yticklabels=labels)
         ax.set_title(val[0], weight='semibold')
 
         ax.tick_params(axis='both', labelsize=3, width=0.3, length=1)
@@ -148,13 +145,12 @@ def multi_plot_histo(corr_dict, labels):
     fig2.subplots_adjust(hspace=.5, wspace=.001)
 
     for ax2, val2 in tqdm(zip(axs2.ravel(), corr_dict.items())):
-        sns.distplot(val2[1], ax=ax2, vertical=True)  # vertical for count on y
+        sns.distplot(val2[1], ax=ax2)
         ax2.set_title(val2[0], weight='semibold')
         total = int(np.sum(val2[1]))
-        ax2.text(0.8, 0.1, join('sum: ' + str(total) + ' (' + str(int(total/(500*500*2)*100)) + '% of total)'))
+        ax2.text(0.85, 0.05, join('sum: ' + str(total) + ' (' + str(int(total/(500*500*2)*100)) + '% of total)'))
     fig2.delaxes(axs2[3, 2])
     plt.show()
-
 
 
 # selects plots in main
@@ -228,14 +224,13 @@ def main(dataset_trained, dataset_extracted, sorted):
 
     # plots on one dataset
     plotter(corr_dict_layer4, labels, dataset_trained, dataset_extracted,
-            single_plot=False, multi_plot=True, rdm_plot=True)
+            single_plot=False, multi_plot=False, rdm_plot=False)
 
     return corr_dict_layer4
 
 
 
 if __name__ == '__main__':
-
     # set device
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
@@ -244,28 +239,25 @@ if __name__ == '__main__':
         device = torch.device("cpu")
         print("Devise used = ", device)
 
-    dataset_trained = 'mnist'  # = dataset_extracted (both source dataset)
+    # set source(trained) and target(extracted) datasets
+    dataset_trained = 'mnist'
     corr_dict_source = main(dataset_trained, dataset_trained, sorted=True)
 
     dataset_extracted = 'fashionmnist'
     corr_dict_target = main(dataset_trained, dataset_extracted, sorted=True)  # on target dataset
 
 
-    # calc model RDM
-    # overlapping dict keys (model names) - add dataset names
-    corr_dict_source = {f'{k}@{dataset_trained}': v for k, v in corr_dict_source.items()}
-    corr_dict_target = {f'{k}@{dataset_extracted}': v for k, v in corr_dict_target.items()}
+    # calculate only diagonal of model RDM (=corr/dist of same model for source and target data)
+    compare_rdm_euclid = calc_target_compare_rdm(distance.euclidean, list(corr_dict_source.values()), list(corr_dict_target.values()))
+    compare_rdm_spearman = calc_target_compare_rdm(spearman_dist, list(corr_dict_source.values()), list(corr_dict_target.values()))
 
-    compare_rdm_euclid = calc_target_compare_rdm(distance.euclidean,
-                                                 list(corr_dict_source.values()),
-                                                 list(corr_dict_target.values()))
-    compare_rdm_spearman = calc_target_compare_rdm(spearman_dist,
-                                                   list(corr_dict_source.values()),
-                                                   list(corr_dict_target.values()))
+    # load Acc to add to plot
+    df = pd.read_pickle(join(os.getcwd(), 'models', dataset_trained, 'df_pre_'+dataset_trained))  #Fix to work generally
+    acc = df[df['seed'] == 1]['pre_test_acc']
 
-    plot_rdm_compare(compare_rdm_euclid, compare_rdm_spearman, list(corr_dict_source.keys()))
-    plt.title(f"Compare RDM values - pre: {dataset_trained} / extracted:{dataset_extracted}, {dataset_trained}",
-              weight='semibold', fontsize=8)
+    plot_rdm_compare(compare_rdm_euclid, compare_rdm_spearman, list(corr_dict_source.keys()), acc)
+    plt.title(f"Compare RDM values - for all models pre:{dataset_trained} on {dataset_extracted} & {dataset_trained}",
+              weight='semibold', fontsize=11.5)
     plt.show()
 
 

sum_of_squares.py
@@ -1,13 +1,11 @@
 import numpy as np
-from scipy.spatial.distance import cdist
 import torch
-from collections import Counter
 
 
 def sum_squared(layer, labels):
+    # convert to numpy if tensor
     if torch.is_tensor(layer):
         layer = torch.Tensor.numpy(layer)
-        #print('tensor passed - converted to numpy')
     if torch.is_tensor(labels):
         labels = torch.Tensor.numpy(labels)
 
@@ -16,58 +14,41 @@ def sum_squared(layer, labels):
     cluster_means = []
     ss = []
     for label in np.unique(labels):
-        #print(label)
         clusters.append(layer[labels == label])
         cluster_means.append(np.mean(clusters[-1], axis=0))
-        ss.append(np.sum(cdist(clusters[-1], np.array([cluster_means[-1]]))))
-        #print(ss)
+        ss.append(np.sum((clusters[-1] - np.array([cluster_means[-1]])) ** 2))
 
     # TSS
     layer_mean = np.mean(layer, axis=0)
-    tss = np.sum(cdist(layer, np.array([layer_mean])))
+    tss = np.sum((layer - np.array([layer_mean]))**2)
 
-    # distances from every cluster mean to the total mean
-    t_mean_dist = cdist(np.array(cluster_means), np.array([layer_mean]))
-    # add dist to mean for nr of pts to SSW cluster
-    ss_total = np.sum(ss)
-    #print(Counter(labels))
-    for key in Counter(labels).keys():
-        ss_total += Counter(labels)[key] * t_mean_dist[key - 1].item()
-    return tss, np.mean(ss), ss_total
+    return tss, np.sum(ss)
+    # then look at ssw/tss: should be as small as possible
+    # (= small ss within clusters, samples tight around cluster mean, & tss large, diff. labels get separated far apart)
 
 
 if __name__ == '__main__':
+
     layer = np.array([[1, 0.8], [1, 2], [3, 4], [4, 5], [4, 2], [7, 7], [5, 5]])
     labels = np.array([1, 1, 2, 2, 2, 3, 3])
-    tss, ss_mean, ss_total = sum_squared(layer, labels)
-
-    print('TSS: ', tss)
-    print('SSW: ', ss_total)
-    print('-> SSW/TSS: ', ss_total / tss)
-    print('compare mean(ss)/tss: ', ss_mean / tss)
+    tss, ss = sum_squared(layer, labels)
+    print('TSS: ', tss, ' SS: ', ss, ' ss/tss: ', ss / tss)
 
     # ex with tensor input
     layer = torch.tensor([[1, 2], [3, 4], [5, 6]])
     labels = np.array([1, 2, 2])
-    tss, ss_mean, ss_total = sum_squared(layer, labels)
-    print(tss, ss_mean, ss_total)
+    tss, ss = sum_squared(layer, labels)
+    print(tss, ss)
 
-
+    # for extracted activations, like in analyze.py
     path = '/mnt/antares_raid/home/bemmerl/thesis/data/mnist2class/models_2/_extracted.pt'
     models = torch.load(path)
-    all_ids = []
-    all_ss = []
     ss = []
-    compare= []
     for name, model in models.items():
-        print(name)
+        # print(name)
         layers = model['layers']  # input + 6 model output layers
         labels = model['labels']
     for layer in layers:
-        #print(layer, labels)
-        tss, ss_mean, ssw = sum_squared(layer, labels)
-        #print(ssw, tss)
+        tss, ssw = sum_squared(layer, labels)
         ss.append(ssw / tss if tss != 0 else 0)
-        compare.append(ss_mean / tss if tss != 0 else 0)
     print(ss)
-    print(compare)