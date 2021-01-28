6e843dd2d467c45b98e629f1b7fabc865d754e6d
Florian Bemmerl <florian.bemmerl@gmail.com>
2021-01-28 13:44:47 +0100

    refactor dataset creation scripts - shorten code and add comments

--- datasets/create_mnist_noise.py
+++ datasets/create_mnist_noise.py
@@ -1,53 +1,52 @@
+"""
+Create mnist_noise dataset by augmenting MNIST dataset to pure random noise
+
+Note: MNIST dataset in /data/mnist needed
+"""
+
 import torch
 import os
 from os.path import join
-import shutil
 import numpy as np
 from train import train_utils
 
 # set seed
 train_utils.set_seed(1)
 
-# Augment MNIST dataset to pure random noise
+# set directories
 dataset_name = 'mnist'
 root_dir = os.getcwd()
 dataset_dir = join(root_dir, '../data', dataset_name)
 
+# new dir for new mnist_noise dataset
+save_dir = join(root_dir, '../data', 'mnist_noise', 'processed')
+# check if directory exists or not yet
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# print('Noise_structured data results directory: ' + save_dir)
+
+
 # augment values of MNIST dataset pictures - random noise
 train_data, train_labels = torch.load(join(dataset_dir, 'processed/training.pt'))
 print(np.shape(train_data))
+# overwrite train_data with random values to create noise samples
 train_data = np.random.uniform(0, 1, (60000, 784))
 
+# same for test with different shape
 test_data, test_labels = torch.load(join(dataset_dir, 'processed/test.pt'))
 print(np.shape(test_data))
 test_data = np.random.uniform(0, 1, (10000, 784))
 
-# train
+
+# reshape into train sample size, convert to tensor and save
 train_data = train_data.reshape((-1, 28, 28))
 train_data = torch.from_numpy(train_data)
 print(type(train_data), train_data.shape)
-# save as torch pt file
-torch.save([train_data, train_labels], 'training.pt')
+torch.save([train_data, train_labels], join(save_dir, 'training.pt'))
 
-# test
+# reshape into test sample size, convert to tensor and save
 test_data = test_data.reshape((-1, 28, 28))
 test_data = torch.from_numpy(test_data)
 print(type(test_data), test_data.shape)
-# save as torch pt file
-torch.save([test_data, test_labels], 'test.pt')
-
-
-# new dir for new mnist_noise dataset
-new_dir = join(root_dir, '../data', 'mnist_noise')
-# check if directory exists or not yet
-if not os.path.exists(new_dir):
-    os.makedirs(new_dir)
-
-save_dir = join(new_dir, 'processed')
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-print('Noise_structured data results directory: ' + save_dir)
-
-# move created files
-shutil.move('training.pt', save_dir)
-shutil.move('test.pt', save_dir)
+torch.save([test_data, test_labels], join(save_dir, 'test.pt'))
--- datasets/create_mnist_splits.py
+++ datasets/create_mnist_splits.py
@@ -1,13 +1,30 @@
+"""
+Create mnist_split datasets: Split MNIST dataset into split1 with labels 0 to 4 and split2 with 5 to 9
+
+
+Note: mnist dataset is not balanced
+labels [0 1 2 3 4 5 6 7 8 9]
+count [5923 6742 5958 6131 5842 5421 5918 6265 5851 5949]
+
+    therefore also in the resulting splits:
+    mnist_split1
+        train : torch.Size([30596]) torch.Size([30596, 28, 28])
+        test : torch.Size([5139]) torch.Size([5139, 28, 28])
+    mnist_split2
+        train : torch.Size([29404]) torch.Size([29404, 28, 28])
+        test : torch.Size([4861]) torch.Size([4861, 28, 28])
+
+Note: MNIST dataset in /data/mnist needed
+"""
+
 import torch
 import os
 from os.path import join
 import numpy as np
 from train import train_utils
-import shutil
 
-###
+# run twice for split1 and split2
 create_dataset = 'mnist_split2'  # 'mnist_split1'
-
 
 # set seed
 train_utils.set_seed(1)
@@ -25,14 +42,11 @@ print(np.shape(test_data), np.shape(test_labels))
 
 
 # new dir for new mnist_noise dataset
-new_dir = join(root_dir, '../data', create_dataset)
-if not os.path.exists(new_dir):
-    os.makedirs(new_dir)
-
-save_dir = join(new_dir, 'processed')
+save_dir = join(root_dir, '../data', create_dataset, 'processed')
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
-print('Mnist Split data results directory: ' + save_dir)
+
+# print('Mnist Split data results directory: ' + save_dir)
 
 train_labels_split = []
 train_data_split = []
@@ -45,52 +59,29 @@ else: labels = [5, 6, 7, 8, 9]
 
 for label in labels:
     idx = train_labels == label
-    print(idx)
-    print(len(train_labels[idx]))
+    # print(idx)
+    # print(len(train_labels[idx]))
     train_labels_split.append(train_labels[idx])
     train_data_split.append(train_data[idx])
-    print(len(train_labels_split[label-5]))
+    # print(len(train_labels_split[label-5]))
 
     idxx = test_labels == label
     test_labels_split.append(test_labels[idxx])
     test_data_split.append(test_data[idxx])
-    print(len(test_labels_split[label-5]))
 
 train_labels_split = torch.cat(train_labels_split)
 train_data_split = torch.cat(train_data_split)
 test_labels_split = torch.cat(test_labels_split)
 test_data_split = torch.cat(test_data_split)
 
-print(train_labels_split.shape, train_data_split.shape)
-print(test_labels_split.shape, test_data_split.shape)
+# print(train_labels_split.shape, train_data_split.shape)
+# print(test_labels_split.shape, test_data_split.shape)
 
 # adj labels for mnist_split2 to fit NN output and to use same implementation as for other datasets
 if create_dataset == 'mnist_split2':
-    print(train_labels_split)
-    train_labels_split = train_labels_split-5
-    print(train_labels_split)
+    train_labels_split = train_labels_split - 5
     test_labels_split = test_labels_split - 5
 
 # save as torch pt file
-torch.save([train_data_split, train_labels_split], 'training.pt')
-torch.save([test_data_split, test_labels_split], 'test.pt')
-
-# move created files
-shutil.move('training.pt', save_dir)
-shutil.move('test.pt', save_dir)
-
-
-
-## mnist not balanced?
-
-# mnist_split1
-# train : torch.Size([30596]) torch.Size([30596, 28, 28])
-# test : torch.Size([5139]) torch.Size([5139, 28, 28])
-
-# mnist_split2
-# train : torch.Size([29404]) torch.Size([29404, 28, 28])
-# test : torch.Size([4861]) torch.Size([4861, 28, 28])
-
-# because mnist torch dataset (training):
-# labels[0 1 2 3 4 5 6 7 8 9]
-# count [5923 6742 5958 6131 5842 5421 5918 6265 5851 5949]
+torch.save([train_data_split, train_labels_split], join(save_dir, 'training.pt'))
+torch.save([test_data_split, test_labels_split], join(save_dir, 'test.pt'))
